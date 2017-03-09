bf88a7a157006b557a317baa83a8adca50a1dedb
Fredrik Jonsson <frjo@xdeb.org>
2017-03-09 13:05:58 +0100

    jQuery, slicknav and production css.

--- themes/zen/static/js/script.js
+++ themes/zen/static/js/script.js
@@ -4,7 +4,7 @@
  */
 
 $(document).ready(function () {
-  if (window.matchMedia) {
+  if (window.matchMedia && $.isFunction($.fn.slicknav)) {
     var mq = window.matchMedia("(max-width: 666px)");
     if (mq.matches) {
       $('.main-menu').hide().find('.navbar').filter(':first').slicknav();
