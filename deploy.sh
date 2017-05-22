d349f1d2553be5763c2095aff27896fa37f744fe
Fredrik Jonsson <frjo@xdeb.org>
2017-05-22 18:14:39 +0200

    Configuration and layouts for xdeb.org.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-hugo && rsync -e 'ssh -ax -p 2222' -avz --human-readable --delete --exclude '.DS_Store' public/ seldon.combonet.se:/var/www/customers/frjo/web/xdeb/beta/
+hugo && rsync -e 'ssh -ax -p 2222' -avz --human-readable --delete --exclude '.DS_Store' --exclude 'reunion' public/ seldon.combonet.se:/var/www/customers/frjo/web/
