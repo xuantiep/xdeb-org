ef70f516c0849b10150b78e10d8011e36c2bacf9
Fredrik Jonsson <frjo@xdeb.org>
2018-01-31 14:16:29 +0100

    Some improvments to pwa files.

--- static/service-worker.js
+++ static/service-worker.js
@@ -6,12 +6,15 @@
 
 'use strict';
 
-// Install stage sets up the index page (home page) in the cahche and opens a new cache.
+// Set this to something unique.
+var cachename = 'xdeb-org-offline';
+
+// Install stage sets up the index page (home page) in the cache and opens a new cache.
 self.addEventListener('install', function(event) {
   var indexPage = new Request('index.html');
   event.waitUntil(
     fetch(indexPage).then(function(response) {
-      return caches.open('zen-pwa-offline').then(function(cache) {
+      return caches.open(cachename).then(function(cache) {
         return cache.put(indexPage, response);
       });
   }));
@@ -20,7 +23,7 @@ self.addEventListener('install', function(event) {
 // If any fetch fails, it will look for the request in the cache and serve it from there first.
 self.addEventListener('fetch', function(event) {
   var updateCache = function(request) {
-    return caches.open('zen-pwa-offline').then(function (cache) {
+    return caches.open(cachename).then(function (cache) {
       return fetch(request).then(function (response) {
         return cache.put(request, response);
       });
@@ -34,7 +37,7 @@ self.addEventListener('fetch', function(event) {
       // Check to see if you have it in the cache.
       // Return response.
       // If not in the cache, then return error page.
-      return caches.open('zen-pwa-offline').then(function (cache) {
+      return caches.open(cachename).then(function (cache) {
         return cache.match(event.request).then(function (matching) {
           var report = !matching || matching.status == 404?Promise.reject('no-match'): matching;
           return report
