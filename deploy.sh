8fe957fb86b586e49698c8e8f30c4d89d1757865
Fredrik Jonsson <frjo@xdeb.org>
2019-01-17 09:55:37 +0100

    Move deploy to eto.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-hugo && rsync -e 'ssh -ax -p 2222' -avz --human-readable --delete --exclude '.DS_Store' --exclude 'reunion' public/ seldon.combonet.se:/var/www/customers/frjo/web/
+hugo && rsync -e 'ssh -ax -p 2222' -avz --human-readable --delete --exclude '.DS_Store' --exclude 'reunion' public/ eto.xdeb.net:/var/www/customers/frjo/web/
