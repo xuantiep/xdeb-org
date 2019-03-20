16e0d45148e35fddd3c74c5b756504740c19956b
Fredrik Jonsson <frjo@xdeb.org>
2019-03-20 05:08:51 +0100

    Testing replacing jquery with tiny umbrella lib, part 2.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,41 +10,25 @@
   // Add a js class to the html-tag when JavsScript is active.
   $('html').removeClass('nojs').addClass('js');
 
-  // Activate the mobil menu for small screens.
-  if (window.matchMedia && $('.mobile-nav').length) {
-    var mq = window.matchMedia('(max-width: 999px)');
-    if (mq.matches) {
-      // Toggle the mobile nav sheet.
-      $('.mobile-nav__cover, .mobile-nav__toggle').handle('click', function () {
-        $('body').scroll().toggleClass('js-nav-open');
-      });
-
-      // Close the nav sheet after click (needed for anchor links).
-      $('.mobile-nav__sheet').find('a').on('click', function () {
-        $('body').removeClass('js-nav-open');
-      });
-    }
-  }
-
   // Add button to pre > code to copy the code to the clipboard.
   if (window.matchMedia && document.queryCommandSupported && document.queryCommandSupported('copy')) {
     var mq2 = window.matchMedia('(min-device-width: 1111px)');
     if (mq2.matches) {
       $('.content').find('pre').each(function (e, i) {
         var codeitem = 'js-code-item-' + i;
-        var button = $('<button data-codeitem="' + codeitem + '"/>').text('Copy code').addClass('js-clipboard-button');
-        $(e).addClass(codeitem).after(button);
+        var $button = $('<button data-codeitem="' + codeitem + '"/>').text('Copy code').addClass('js-clipboard-button');
+        $(e).addClass(codeitem).after($button);
       });
 
       $('.js-clipboard-button').handle('click', function () {
         var codeitem = '.' + $(this).data('codeitem');
         var codesnippet = $(codeitem).find('code').html();
-        var textarea = $('<textarea id="codesnippet">').html(codesnippet).addClass('visually-hidden');
-        $('body').append(textarea);
-        document.getElementById('codesnippet').focus();
-        document.getElementById('codesnippet').select();
+        var $textarea = $('<textarea>').html(codesnippet).addClass('visually-hidden');
+        $('body').append($textarea);
+        $textarea.first().focus();
+        $textarea.first().select();
         document.execCommand('copy');
-        textarea.remove();
+        $textarea.remove();
       });
     }
   }
@@ -57,4 +41,3 @@
   }
 
 })(u);
-
