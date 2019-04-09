9aca2ddf5a34804b18902fc72cca4995cb277899
Fredrik Jonsson <frjo@xdeb.org>
2019-04-09 18:27:14 +0200

    Added ignore-existing to deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-hugo --minify && rsync -e 'ssh -ax -p 2222' -avz --human-readable --delete --exclude '.DS_Store' --exclude 'reunion' public/ eto.xdeb.net:/var/www/customers/frjo/web/
+hugo --minify && rsync -e 'ssh -ax -p 2222' --archive --delete --verbose --compress --ignore-existing --human-readable --exclude '.DS_Store' --exclude 'reunion' public/ eto.xdeb.net:/var/www/customers/frjo/web/
