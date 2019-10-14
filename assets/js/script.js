9049a291dcc73961a99615ccf88f73a5c67905bc
Fredrik Jonsson <frjo@xdeb.org>
2019-10-14 11:04:11 +0200

    Make the code copy button work with pygment.

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,16 +14,22 @@
   if (window.matchMedia && document.queryCommandSupported && document.queryCommandSupported('copy')) {
     var mq2 = window.matchMedia('(min-device-width: 1111px)');
     if (mq2.matches) {
+      // Strip html tags from text.
+      function strip(html){
+        var doc = new DOMParser().parseFromString(html, 'text/html');
+        return doc.body.textContent || "";
+      }
+
       $('.content').find('pre').each(function (e, i) {
         var codeitem = 'js-code-item-' + i;
         var $button = $('<button data-codeitem="' + codeitem + '"/>').text('Copy code').addClass('js-clipboard-button');
-        $(e).addClass(codeitem).after($button);
+        $(e).addClass('js-code-item').addClass(codeitem).after($button);
       });
 
       $('.js-clipboard-button').handle('click', function () {
         var codeitem = '.' + $(this).data('codeitem');
         var codesnippet = $(codeitem).find('code').html();
-        var $textarea = $('<textarea>').html(codesnippet).addClass('visually-hidden');
+        var $textarea = $('<textarea>').html(strip(codesnippet)).addClass('visually-hidden');
         $('body').append($textarea);
         $textarea.first().focus();
         $textarea.first().select();
