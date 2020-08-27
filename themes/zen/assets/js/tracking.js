8d24b2147040a8580bd4c53b6f74a7b55750767a
Fredrik Jonsson <frjo@xdeb.org>
2020-08-27 08:31:45 +0200

    Updated the zen theme.

--- themes/zen/assets/js/tracking.js
+++ themes/zen/assets/js/tracking.js
@@ -1,9 +1,9 @@
 /**
  * @file
- * A JavaScript file for matomo analytic tracking.
+ * A JavaScript file for analytic tracking.
  */
 
-var idSite = {{ .Site.Params.piwikSiteID }};
+var idSite = '{{ .Site.Params.piwikSiteID }}';
 var matomoTrackingApiUrl = 'https://{{ .Site.Params.piwikTrackerUrl }}/matomo.php';
 
 var _paq = window._paq || [];
@@ -11,3 +11,9 @@ _paq.push(['setTrackerUrl', matomoTrackingApiUrl]);
 _paq.push(['setSiteId', idSite]);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
+
+var googleAnalytics = '{{ .Site.GoogleAnalytics }}';
+
+var _gaq = window._gaq || [];
+_gaq.push(['_setAccount', googleAnalytics]);
+_gaq.push(['_trackPageView']);
