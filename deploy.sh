72ac1fb3e61b224b270d97f44fb1563367688191
Fredrik Jonsson <frjo@xdeb.org>
2021-03-08 19:42:46 +0100

    Deploy to seldon for now.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-hugo --cleanDestinationDir && rsync -e 'ssh -ax -p 2222' --archive --delete --verbose --compress --human-readable --exclude '.DS_Store' --exclude 'reunion' public/ eto.xdeb.net:/var/www/customers/frjo/web/
+hugo --cleanDestinationDir && rsync -e 'ssh -ax -p 2222' --archive --delete --verbose --compress --human-readable --exclude '.DS_Store' --exclude 'reunion' public/ seldon.combonet.se:/var/www/customers/frjo/web/
