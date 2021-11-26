89f3c9ef778eaf7cdca07d48da06f76b13d47244
Fredrik Jonsson <frjo@xdeb.org>
2021-11-26 11:19:36 +0100

    Added animation and timout to copy to clipboard js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,22 +18,28 @@
     var mq2 = window.matchMedia('(min-device-width: 1111px)');
     if (mq2.matches) {
 
-      $('.content').find('pre').each(function (e, i) {
-        var codeitem = 'js-code-item-' + i;
-        var $button = $('<button data-codeitem="' + codeitem + '"/>').text('Copy code').addClass('js-clipboard-button');
-        $(e).addClass('js-code-item').addClass(codeitem).after($button);
-      });
+      setTimeout(function() {
+        $('.content').find('pre').each(function (e, i) {
+          var codeitem = 'js-code-item-' + i;
+          var $button = $('<button data-codeitem="' + codeitem + '"/>').text('Copy code').addClass('js-clipboard-button');
+          $(e).addClass('js-code-item').addClass(codeitem).after($button);
+        });
 
-      $('.js-clipboard-button').handle('click', function () {
-        var codeitem = '.' + $(this).data('codeitem');
-        var codesnippet = $(codeitem).find('code').html();
-        var $textarea = $('<textarea>').html(strip(codesnippet)).addClass('visually-hidden');
-        $('body').append($textarea);
-        $textarea.first().focus();
-        $textarea.first().select();
-        document.execCommand('copy');
-        $textarea.remove();
-      });
+        $('.js-clipboard-button').handle('click', function () {
+          var codeitem = '.' + $(this).data('codeitem');
+          var codesnippet = $(codeitem).find('code').html();
+          var $textarea = $('<textarea>').html(strip(codesnippet)).addClass('visually-hidden');
+          $('body').append($textarea);
+          $textarea.first().focus();
+          $textarea.first().select();
+          $(codeitem).addClass('flash-item');
+          document.execCommand('copy');
+          $textarea.remove();
+          setTimeout(function() {
+            $(codeitem).removeClass('flash-item');
+          }, 700);
+        });
+      }, 10);
     }
   }
 
